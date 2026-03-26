2a29a3392c31be00b5c76841ca8125ec927d5135
Empanada442 <juanma150905@gmail.com>
2026-03-26 13:02:03 -0500

    conexion crud terminada

--- crud/src/app/api/tasks/[id]/route.js
+++ crud/src/app/api/tasks/[id]/route.js
@@ -1,24 +1,43 @@
 import { NextResponse } from "next/server";
+import { prisma } from "@/libs/prisma";
 
 export async function GET(request, { params }) {
     const { id } = await params;
 
+    const tasksGet = await prisma.task.findUnique({
+        where: { id: Number(id) },
+    });
+
+    console.log(tasksGet);
     return NextResponse.json({
         message: "GET request received " + id
     });
 }
 
 export async function PUT(request, { params }) {
-    const { id } = await params;
+    const { id } = params;
+    const body = await request.json();
 
+    const updatedTask = await prisma.task.update({
+        where: { id: Number(id) },
+        data: body,
+    });
+
+    console.log(updatedTask);
     return NextResponse.json({
-        message: "PUT request received " + id
+        message: "Task updated successfully",
+        task: updatedTask,
     });
 }
 
 export async function DELETE(request, { params }) {
     const { id } = await params;
 
+    const tasksDelete = await prisma.task.delete({
+        where: { id: Number(id) },
+    });
+
+    console.log(tasksDelete);
     return NextResponse.json({
         message: "DELETE request received " + id
     });
--- crud/src/app/api/tasks/route.js
+++ crud/src/app/api/tasks/route.js
@@ -13,8 +13,16 @@ export  async function GET() {
   });
 }
 
-export function POST() {
-    return NextResponse.json({
-      message: "subido correctamente",
-    });
-  }
+export async function POST(request) {
+  const data = await request.json();
+  await prisma.task.create({
+    data: {
+      title: data.title,
+      description: data.description,
+      userId: String(1), // Asignar un userId fijo para esta tarea
+    },
+  });
+  return NextResponse.json({
+    message: "Tarea creada correctamente",
+  });
+}
